2f18ef9ce67813780d8c51e149b4552b4359180b
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-02-25 02:22:34 -0500

    updated test

--- site/test.php
+++ site/test.php
@@ -1,6 +1,6 @@
 <?php
 require 'database.php';
-
+require './tracker/functions.reopentracker.php';
 
 $sql = "SELECT DISTINCT info_hash FROM peers";
 
@@ -8,7 +8,8 @@ $stmt = $pdo -> query($sql);
 $files = array();
 
 while($row = $stmt ->fetch(PDO::FETCH_ASSOC)) {
-	echo $row['info_hash'];
+	$values[0][] = array('key' => ':hash', 'value' => $hash, 'type' => PDO::PARAM_STR);
+	echo bdecode($row['info_hash']);
 	echo "<br />";
 }
 
