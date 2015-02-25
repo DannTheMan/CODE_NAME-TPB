3db2ceefee9a7ed75ee12fa94f106735d57128c7
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 05:55:06 -0500

    a

--- site/testing.php
+++ site/testing.php
@@ -1,69 +1,43 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-        <title>Codename: TPB</title>
-        <link rel="icon" href="resources/boatIcon.jpg" type="image/x-icon">
+		<title>Codename: TPB</title>
+		<link rel="icon" href="resources/boatIcon.jpg" type="image/x-icon">
 		<link href="landing.css" type="text/css" rel="stylesheet" />
 		<script language="JavaScript" src="landing.js"></script>
 	</head>
 
 	<body>
 
-		<?php unset($_COOKIE[crypt("username", "askdalkweasdaaowej312sa9")]);
-    $ss = "123";
-    $uname = "jimmy";
-    unset($_COOKIE[crypt("username", "askdalkweasdaaowej312sa9")]);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "cntpb";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn -> connect_error) {
-        die("Connection failed: " . $conn -> connect_error);
-    }
-    $sql = "SELECT hash, username FROM users WHERE username='$uname'";
-    $sid = mysqli_query($conn, $sql);
-    $name = "";
-    $ans = false;
-    if (mysqli_num_rows($sid) == 0) {
-        echo('false');
-    } else {
-        while ($found = mysqli_fetch_array($sid)) {
-            $f0 = preg_replace('/[^[:print:]]/', '', $found[0]);
-            if (strcmp($f0, $ss) == 0) {$ans = true;
-                $name = $found[1];
-            }
-        }
-
-        if ($ans) {
-            // 86400 = 1 day
-        }
-
-        if ($ans) {
-            echo('true');
-        } else
-            echo('false');
-    }
-    echo("<br><br>$name <br><br>");
-		?>
-
 		<?php
-        //unset($_COOKIE[crypt("username","askdalkweasdaaowej312sa9")]);
-        //$ss = crypt(htmlspecialchars($_POST["secure"]), "1241asda0");
-        //$uname = htmlspecialchars($_POST["username"]);
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "cntpb";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn -> connect_error) {
-            die("Connection failed: " . $conn -> connect_error);
-        }
-        $sql = "SELECT username, id FROM users";
-        $sid = mysqli_query($conn, $sql);
-        while ($i = mysqli_fetch_array($sid)) {
-            echo($i[0] . ", " . $i[1] . ":K" . "<br>");
-        }
+require 'database.php';
+require dirname(__FILE__).'/../tracker/functions.reopentracker.php'
+		?>
+		<?php
+
+        //$name = htmlspecialchars($_POST["n"]);
+        ///$desc = htmlspecialchars($_POST["d"]);
+
+        //$ff = new SplFileObject($_POST["f"]);
+        $name = "megatest";
+        $desc = "a new test";
+        $f = "tester.txt";
+
+        $file = $ff;
+
+        $file_string = file_get_contents($file);
+        $hash_info = sha1(bencode(bdecode($file_string)['info']));
+
+        $sql = "INSERT INTO torrents (name,description,file) VALUES ('$name','$desc','$file')";
+        //put real SQL stuff here
+        $sid = $pdo -> prepare($sql);
+        $sid -> execute();
+
+        echo('hash_info: ' . $hash_info . '\n');
+        echo('file: ' . $file . '\n');
+        echo('file_string: ' . $file_string . '\n');
+
+        //echo("true");
 		?>
 	</body>
 </html>
