a71c1ec30aabc1c6c3a81c13b73f7106aeaad807
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 17:47:04 -0500

    a

--- site/landing.php
+++ site/landing.php
@@ -66,6 +66,66 @@
 			</p><ul id="results"></ul>
 			</div-->
 		</div>
+		
+		<div id="statsBox">
+                <h2>Website Statistics:</h2>
+                <!--form action="search.php">
+                    Search files by name:
+                    <br>
+                    <input type="text" name="searchtext" id="textfield" required>
+                    <br>
+                    <br>
+                    <input type="submit">
+                </form-->
+                <?php
+                echo("Number of registered users: ");
+                $users = 0;
+                foreach ($pdo->query("SELECT COUNT(id) FROM users") as $rowi) {
+                            $users = $rowi[0];
+                            break;
+                        }
+                        echo($users);
+                echo("<br><br>Number of files uploaded: ");
+                $files = 0;
+                foreach ($pdo->query("SELECT COUNT(id) FROM torrents") as $rowi) {
+                            $files = $rowi[0];
+                            break;
+                        }
+                        echo($file);
+                echo("<br><br>Total number of seeders: ");
+                $seeders = 0;
+                        foreach ($pdo->query("SELECT COUNT(p.id) FROM peers p, WHERE p.remaining = 0 AND p.uploaded > p.downloaded") as $rowi) {
+                            $seeders = $rowi[0];
+                            break;
+                        }
+                echo($seeders);
+                echo("<br><br>Total number of leechers: ");
+                $leechers = 0;
+                        foreach ($pdo->query("SELECT COUNT(p.id) FROM peers as p") as $rowi) {
+                            $leechers = $rowi[0];
+                            $leechers = $leechers - $seeders;
+                            break;
+                        }
+                echo($leechers);
+                echo("<br><br>Seeder/Leecher ratio: ".((($seeders/$leechers)*100)."%"));
+                echo("<br><br>Total amount of data uploaded: ");
+                $bytes=0;
+                foreach ($pdo->query("SELECT SUM(p.downloaded) FROM peers as p") as $rowi) {
+                            $bytes = $rowi[0];
+                            break;
+                        }
+                echo($bytes." bytes");
+                ?>
+                <br>
+            </div>
+            <!--div id="resultsBox">
+            <h2>Results:</h2>
+            <br>
+            <p id="resultsMessage">
+            No results found with your current search.
+            </p><ul id="results"></ul>
+            </div-->
+        </div>
 
 	</body>
 </html>
