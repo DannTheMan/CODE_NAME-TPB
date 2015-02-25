be5c06d479f483a1bbb536e4c2d9bb21dcfd3440
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-02-25 10:34:27 -0500

    d

--- data-base/CNTPB_database.sql
+++ data-base/CNTPB_database.sql
@@ -10,13 +10,29 @@ CREATE TABLE users
 	UNIQUE KEY username (username)
 );
 
+DROP TABLE IF EXISTS peers;
+CREATE TABLE `peers` (
+    `id` INT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,    
+    `info_hash` BINARY(20) NOT NULL, 
+    `ip` INT(11) NOT NULL, `port` INT(5) NOT NULL, 
+    `peer_id` BINARY(20) NOT NULL, 
+    `uploaded` INT(20) NOT NULL DEFAULT 0, 
+    `downloaded` INT(20) NOT NULL DEFAULT 0, 
+    `remaining` INT(20) NOT NULL DEFAULT 0, 
+    `update_time` INT NOT NULL, 
+    `expire_time` INT NOT NULL,
+    UNIQUE KEY info_hash (info_hash)
+);
+
 CREATE TABLE torrents
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	file mediumblob NOT NULL,
+	info_hash BINARY(20) NOT NULL,
 	description text NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (info_hash) REFERENCES peers(info_hash)
 );
 
 CREATE TABLE user_torents
@@ -38,19 +54,6 @@ CREATE TABLE comments
 	PRIMARY KEY(id),
 	FOREIGN KEY (user_id) REFERENCES users(id),
 	FOREIGN KEY (torrent_id) REFERENCES torrents(id)
-);
-
-DROP TABLE IF EXISTS peers;
-CREATE TABLE `peers` (
-    `id` INT(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,    
-    `info_hash` BINARY(20) NOT NULL, 
-    `ip` INT(11) NOT NULL, `port` INT(5) NOT NULL, 
-    `peer_id` BINARY(20) NOT NULL, 
-    `uploaded` INT(20) NOT NULL DEFAULT 0, 
-    `downloaded` INT(20) NOT NULL DEFAULT 0, 
-    `remaining` INT(20) NOT NULL DEFAULT 0, 
-    `update_time` INT NOT NULL, 
-    `expire_time` INT NOT NULL 
 );
 
 CREATE FUNCTION saltedHash(username VARCHAR(255), password VARCHAR(255))
