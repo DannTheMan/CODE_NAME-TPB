0a797dca5b8534f7263d41164400cd866bd228e2
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-02-25 02:01:10 -0500

    added test page

--- site/test.php
+++ site/test.php
@@ -4,6 +4,14 @@ require 'database.php';
 
 $sql = "SELECT DISTINCT info_hash FROM peers";
 
+$stmt = $pdo -> query($sql);
+$files = array();
+
+while($row = $stmt ->fetch(PDO::FETCH_ASSOC)) {
+	echo $row['info_hash'];
+}
+
+
 
 
 ?>
