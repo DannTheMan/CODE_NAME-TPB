8cd0d2fe09660c9daf0c8a24182f1d216b6398cb
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 06:50:54 -0500

    a

--- site/uploadBack.php
+++ site/uploadBack.php
@@ -16,14 +16,14 @@ $file = file_get_contents($fileo);//$ff;
 
 echo("  K $name K  ");
 //$file_string = file_get_contents($file);
-$hash_info = sha1(bencode(bdecode($file)['info']));
+//$hash_info = sha1(bencode(bdecode($file)['info']));
 
 $sql = "INSERT INTO torrents (name,description,file) VALUES ('$name','$desc','$file')";//put real SQL stuff here
 $sid = $pdo->prepare($sql);
 $sid->execute();
 
-echo('hash_info: '.$hash_info.'\n');
-//echo('file: '.$file.'\n');
+//echo('hash_info: '.$hash_info.'\n');
+echo('file: '.$file.'\n');
 //echo("<script type=\"text/javascript\">gohome($hash_info);</script>");
 //echo('file_string: '.$file_string.'\n');
 
