280a91d007afc38077a1b6557c9953f9c697ea29
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 08:43:23 -0500

    a

--- site/killYourself.js
+++ site/killYourself.js
@@ -4,9 +4,10 @@ window.onload = function() {
 				url : "killYourself.php",
 				type : 'POST',
 				success : function(data) {
+					window.location.href = "landing.php";
 					location.reload();
 				},
 				});
-	}
-}
+	};
+};
 
