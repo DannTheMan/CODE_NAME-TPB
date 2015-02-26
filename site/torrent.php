a1e4842b5c605e155b88df87142f09237f3ac7e2
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 23:00:41 -0500

    a

--- site/torrent.php
+++ site/torrent.php
@@ -87,7 +87,7 @@
 				?>
 				<h2><?php echo $name ?></h2>
 				<?php echo("<a href=\"downloadFile.php?id=$s\">Click here to download!</a>") ?>
-				<h4>Seeders: <?php echo $seeders; ?> &nbsp;&nbsp;&nbsp;&nbsp;
+				<h4>Seeders: <?php echo $seeders; ?> 
 					Leechers: <?php echo $leechers; ?></h4>
 				<br />
 				<br>
