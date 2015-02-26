508364448e928df69cbfbda1e1cac12bf5d5f6fe
Jared H <hoffmaj2@rose-hulman.edu>
2015-02-25 19:21:15 -0500

    a

--- site/profileBack.php
+++ site/profileBack.php
@@ -1,16 +1,16 @@
 <?php require 'database.php'; ?>
 <?php
 	$uid = $_POST['uid'];
-	if (isset($_POST['name']) {
+	if (isset($_POST['name'])) {
 		$name = $_POST['name'];
 		$dbo->query("UPDATE user_profiles SET name = \"$name\" WHERE uid = \"$uid\"");
-	} else if (isset($_POST['email']) {
+	} elseif (isset($_POST['email'])) {
 		$email = $_POST['email'];
 		$dbo->query("UPDATE user_profiles SET email = \"$email\" WHERE uid = \"$uid\"");
-	} else if (isset($_POST['age']) {
+	} elseif (isset($_POST['age'])) {
 		$age = $_POST['age'];
 		$dbo->query("UPDATE user_profiles SET age = \"$age\" WHERE uid = \"$uid\"");
-	} else if (isset($_POST['gender']) {
+	} elseif (isset($_POST['gender'])) {
 		$gender = $_POST['gender'];
 		$dbo->query("UPDATE user_profiles SET gender = \"$gender\" WHERE uid = \"$uid\"");
 	}
